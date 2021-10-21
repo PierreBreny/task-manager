abd2d2ce68ef8a861e807e90488ae361e1cda52a
Pierre Breny <pierre.breny@gmail.com>
2021-10-21 11:00:40 +0200

    just push

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
+import PropTypes from "prop-types"
 import Button from "./Button"
 
-const Header = () => {
+const Header = ({ title, onAdd }) => {
+    const onClick = () => {
+        console.log("Click")
+    }
+
     return (
         <header className="header">
-        <h1>Task Manager</h1>
-        <Button color="black" text="Add"/>
+            <h1>{title}</h1>
+            <Button color="black" text="Add" onClick={onClick} />
         </header>
     )
+}
+
+Header.defaultProps = {
+    title: "Task Manager",
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
